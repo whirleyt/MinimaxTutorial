8b062295b53d116f0ec910abfbac78a82311e780
Tara Whirley <whirleyt@outlook.com>
2024-04-29 03:12:53 -0400

    Alpha-beta Pruning Quiz In Progress

---
+++ static/js/alphabetaQuiz.js
@@ -0,0 +1,246 @@
+$(document).ready(function() {
+    const alphabetaValues= {
+        1: { answer: ["-inf", "inf"], hint: "<div><p>Values are initialized at -∞ and ∞.</p></div>"},
+        2: { answer: ["-inf", "inf"], hint: "<div><p>No values have been seen to update alpha or beta.</p></div>"},
+        3: { answer: ["-inf", "inf"], hint: "<div><p>No values have been seen to update alpha or beta.</p></div>"},
+        4: { answer: ["8", "inf"], hint: "<div><div><p>The algorithm has seen the value 8.</p></div><div><p>This is a max node, so if the value seen is greater than the current alpha, alpha should be updated.</p></div></div>"},
+        5: { answer: ["-inf", "8"], hint: "<div><div><p>Alpha and beta values are only passed down, starting at -∞, ∞.</p></div><div><p>The value 8 is seen by a min node, so if it is less than the current beta, update beta.</p></div></div>"},
+        6: { answer: ["-inf", "8"], hint: "<div><p>Alpha and beta values get passed down until updated by seeing a value.</p></div>"},
+        7: { answer: ["50", "8"], hint: "<div><div><p>The algorithm has seen the value 50.</p></div><div><p>This is a max node, so if the value seen is greater than the current alpha, alpha should be updated.</p></div><div><p>Is β ≤ α? If so, prune!</p></div></div>"},
+        8: { answer: ["-inf", "8"], hint: "<div><div><p>Alpha and beta values are only passed down, starting at -∞, ∞.</p></div><div><p>The algorithm has encountered the value 8 and it is seen by a min node, so if it is less than the current beta, update beta.</p></div></div>"},
+        9: { answer: ["-inf", "8"], hint: "<div><p>Alpha and beta values get passed down until updated by seeing a value.</p></div>"},
+        10: { answer: ["-inf", "8"], hint: "<div><p>Alpha and beta values get passed down until updated by seeing a value.</p></div>"},
+        11: { answer: ["2", "8"], hint: "<div><div><p>The algorithm has seen the value 2.</p></div><div><p>This is a max node, so if the value seen is greater than the current alpha, alpha should be updated.</p></div></div>"},
+        12: { answer: ["-inf", "8"], hint: "<div><div><p>Alpha and beta values are only passed down, starting at -∞, 8.</p></div><div><p>The value 9 is seen by a min node. If the value seen is less than the current beta, update beta.</p></div></div>"},
+        13: { answer: ["-inf", "8"], hint: "<div><p>Alpha and beta values get passed down until updated by seeing a value.</p></div>"},
+        14: { answer: ["14", "8"], hint: "<div><div><p>The algorithm has seen the value 14.</p></div><div><p>This is a max node, so if the value seen is greater than the current alpha, alpha should be updated.</p></div><div><p>Is β ≤ α? If so, prune!</p></div></div>"}
+    }
+
+
+    let currentQuestion = 2;
+    updateHint(currentQuestion);
+
+    var alphabetaAnswers = {
+                question1: {alpha: [], beta: []},
+                question2: {alpha: [], beta: []},
+                question3: {alpha: [], beta: []},
+                question4: {alpha: [], beta: []},
+                question5: {alpha: [], beta: []},
+                question6: {alpha: [], beta: []},
+                question7: {alpha: [], beta: []},
+                question8: {alpha: [], beta: []},
+                question9: {alpha: [], beta: []},
+                question10: {alpha: [], beta: []},
+                question11: {alpha: [], beta: []},
+                question12: {alpha: [], beta: []},
+                question13: {alpha: [], beta: []},
+                question14: {alpha: [], beta: []},
+              };
+
+    let score= 100;
+
+    for(let i=2; i<=14; i++){
+              $('#alpha-input-'+ i).on('input', function() {
+              console.log(score)
+                    var inputVal = $(this).val();
+
+                    var correctUpdate2 = $('#correct-update-2');
+                    var incorrectUpdate2 = $('#incorrect-update-2');
+                    correctUpdate2.removeClass('show-animation');
+                    incorrectUpdate2.removeClass('show-animation');
+                    var newIncorrectUpdate2 = incorrectUpdate2.clone(true);
+                    incorrectUpdate2.before(newIncorrectUpdate2);
+                    $("." + incorrectUpdate2.attr("class") + ":last").remove();
+
+                    var incorrectUpdate2 = $('#incorrect-update-2');
+                    incorrectUpdate2.css('visibility', 'hidden');
+                    incorrectUpdate2[0].offsetHeight;
+
+
+                    if(inputVal != '') {
+                    alphabetaAnswers['question' + i].alpha.push(inputVal);
+                    if(i==7 || i== 14){
+                        if(inputVal.length === 2){
+                            if(inputVal == alphabetaValues[i].answer[0]) {
+                                $(this).prop('disabled', true);
+                                $(this).css('border', '2px solid #b0b0b0');
+                                $('#correct-update-2').addClass('show-animation');
+                                $('#beta-input-' + (i)).prop('disabled', false);
+                                $('#beta-input-' + i).focus();
+                            } else {
+                                $(this).css('border', '2px solid red');
+                                score-= 1;
+                                $('#incorrect-update-2').addClass('show-animation');
+                            }
+                        } else if(inputVal.length==1) {
+                            const str= String(alphabetaValues[i].answer[0]);
+                            const substr= str.substring(0, 1);
+                            if(substr !== inputVal){
+                                $(this).css('border', '2px solid red');
+                                score-= 1;
+                                $('#incorrect-update-2').addClass('show-animation');
+                            }
+                       }
+                    } else if(i==1 || i== 2 || i==3 || i== 5 || i==6 || i== 8 || i==9 || i== 10 || i==12 || i== 13){
+                        if(inputVal.length === 4){
+                            if(inputVal == alphabetaValues[i].answer[0]) {
+                                $(this).prop('disabled', true);
+                                $(this).css('border', '2px solid #b0b0b0');
+                                $('#correct-update-2').addClass('show-animation');
+                                $('#beta-input-' + (i)).prop('disabled', false);
+                                $('#beta-input-' + i).focus();
+                                var pointer = $('.pointer');
+                                pointer.css('visibility', 'hidden');
+                            } else {
+                                $(this).css('border', '2px solid red');
+                                score-= 1;
+                                $('#incorrect-update-2').addClass('show-animation');
+                                }
+                        } else if(inputVal.length==3) {
+                            const str= String(alphabetaValues[i].answer[0]);
+                            const substr= str.substring(0, 3);
+                            if(substr !== inputVal){
+                               $(this).css('border', '2px solid red');
+                               score-= 1;
+                               $('#incorrect-update-2').addClass('show-animation');
+                            }
+                        } else if(inputVal.length==2) {
+                            const str= String(alphabetaValues[i].answer[0]);
+                            const substr= str.substring(0, 2);
+                            if(substr !== inputVal){
+                                $(this).css('border', '2px solid red');
+                                score-= 1;
+                                $('#incorrect-update-2').addClass('show-animation');
+                            }
+                        }
+                        else if(inputVal.length==1) {
+                            const str= String(alphabetaValues[i].answer[0]);
+                            const substr= str.substring(0, 1);
+                            if(substr !== inputVal){
+                                $(this).css('border', '2px solid red');
+                                score-= 1;
+                                $('#incorrect-update-2').addClass('show-animation');
+                            }
+                        }
+                    } else {
+                        if(inputVal == alphabetaValues[i].answer[0]) {
+                            $(this).prop('disabled', true);
+                            $(this).css('border', '2px solid #b0b0b0');
+                            $('#correct-update-2').addClass('show-animation');
+                            $('#beta-input-' + (i)).prop('disabled', false);
+                            $('#beta-input-' + i).focus();
+                        } else {
+                            $(this).css('border', '2px solid red')
+                            score-= 1;
+                            $('#incorrect-update-2').addClass('show-animation');
+                        }
+                    }
+                    }
+              })
+              $('#beta-input-'+ i).on('input', function() {
+              console.log(score)
+                    var inputVal = $(this).val();
+
+                    var correctUpdate2 = $('#correct-update-2');
+                    var incorrectUpdate2 = $('#incorrect-update-2');
+                    correctUpdate2.removeClass('show-animation');
+                    incorrectUpdate2.removeClass('show-animation');
+                    var newIncorrectUpdate2 = incorrectUpdate2.clone(true);
+                    incorrectUpdate2.before(newIncorrectUpdate2);
+                    $("." + incorrectUpdate2.attr("class") + ":last").remove();
+
+                    var incorrectUpdate2 = $('#incorrect-update-2');
+                    incorrectUpdate2.css('visibility', 'hidden');
+                    incorrectUpdate2[0].offsetHeight;
+
+
+
+                    if(inputVal != '') {
+                    alphabetaAnswers['question' + i].beta.push(inputVal);
+                    if(i==1 || i== 2 || i==3 || i== 4){
+                        if(inputVal.length === 3){
+                            if(inputVal == alphabetaValues[i].answer[1]) {
+                                $(this).prop('disabled', true);
+                                $(this).css('border', '2px solid #b0b0b0');
+                                $('#correct-update-2').addClass('show-animation');
+                                $('#alpha-input-' + (i+1)).prop('disabled', false);
+                                $('#alpha-input-' + (i+1)).focus();
+                                let currentQuestion = i+1;
+                                updateHint(currentQuestion);
+                                var hintSection = $('.hint-text');
+                                hintSection.hide();
+                            } else {
+                                $(this).css('border', '2px solid red');
+                                score-= 1;
+                                $('#incorrect-update-2').addClass('show-animation');
+                            }
+                        } else if(inputVal.length==2) {
+                            const str= String(alphabetaValues[i].answer[1]);
+                            const substr= str.substring(0, 2);
+                            if(substr !== inputVal){
+                                $(this).css('border', '2px solid red');
+                                score-= 1;
+                                $('#incorrect-update-2').addClass('show-animation');
+                            }
+                       } else if(inputVal.length==1) {
+                            const str= String(alphabetaValues[i].answer[1]);
+                            const substr= str.substring(0, 1);
+                            if(substr !== inputVal){
+                                $(this).css('border', '2px solid red');
+                                score-= 1;
+                                $('#incorrect-update-2').addClass('show-animation');
+                            }
+                       }
+                    } else {
+                        if(inputVal == alphabetaValues[i].answer[1]) {
+                            $(this).prop('disabled', true);
+                            $(this).css('border', '2px solid #b0b0b0');
+                            $('#correct-update-2').addClass('show-animation');
+                            $('#alpha-input-' + (i+1)).prop('disabled', false);
+                            $('#alpha-input-' + (i+1)).focus();
+                            let currentQuestion = i+1;
+                            updateHint(currentQuestion);
+                            var hintSection = $('.hint-text');
+                            hintSection.hide();
+                        } else {
+                            $(this).css('border', '2px solid red')
+                            score-= 1;
+                            $('#incorrect-update-2').addClass('show-animation');
+                        }
+                    }
+                    }
+              })
+          }
+
+    function updateHint(questionNum) {
+        if(questionNum <= 14) {
+            var hintHTML = alphabetaValues[questionNum].hint;
+            $('.hint-text').html(hintHTML);
+        } else {
+            displayResults();
+        }
+    }
+
+    $('.hint-icon').click(function() {
+        var hintSection = $('.hint-text');
+        if(hintSection.is(':visible')) {
+            hintSection.hide();
+        } else {
+            hintSection.show();
+        }
+    });
+
+    function displayResults(){
+        setTimeout(function() {
+            $('.see-your-results').css('display', 'inline');
+           }, 2000)
+       }
+
+       $('#resultLink').click(function() {
+            $('.see-your-results').css('display', 'none');
+            $('#resultsText').html('<h6>You Scored ' + score  + '%</h6>');
+//          $('#next-container').css('display', 'block');
+            $('.alphabeta-quiz-container').css('box-shadow', '0px 0px 10px #76c893');
+      })
+
+ })
---
+++ static/js/minimaxBasicsQuiz.js
@@ -0,0 +1,146 @@
+$(document).ready(function() {
+    const minimaxValues= [8, 50, 8, 9, 14, 9, 9]
+
+    var minimaxValueAnswers = {
+            question1: [],
+            question2: [],
+            question3: [],
+            question4: [],
+            question5: [],
+            question6: [],
+            question7: [],
+          };
+
+          let score= 100;
+
+          for(let i=1; i<=minimaxValues.length; i++){
+              $('.minimax-value-'+ i).on('input', function() {
+                    var inputVal = $(this).val();
+                    var correctUpdate = $('#correct-update');
+                    var incorrectUpdate = $('#incorrect-update');
+                    correctUpdate.removeClass('show-animation');
+                    incorrectUpdate.removeClass('show-animation');
+                    var newIncorrectUpdate = incorrectUpdate.clone(true);
+                    incorrectUpdate.before(newIncorrectUpdate);
+                    $("." + incorrectUpdate.attr("class") + ":last").remove();
+
+                    var incorrectUpdate = $('#incorrect-update');
+                    incorrectUpdate.css('visibility', 'hidden');
+                    incorrectUpdate[0].offsetHeight;
+
+
+
+                    if(inputVal != '') {
+                    minimaxValueAnswers['question' + i].push(inputVal);
+                    if(i==2 || i== 5){
+                        if(inputVal.length === 2){
+                            if(inputVal == minimaxValues[i-1]) {
+                                $(this).prop('disabled', true);
+                                $(this).css('border', '2px solid grey');
+                                $('#correct-update').addClass('show-animation');
+                                $('#pointer-'+ i).css('visibility', 'hidden');
+                                $('#pointer-info-'+ i).css('visibility', 'hidden');
+                                createNewPointer(i+1)
+                                $('.minimax-value-' + (i+1)).prop('disabled', false);
+                                $('.minimax-value-' + (i+1)).focus();
+                            } else {
+                                $(this).css('border', '2px solid red');
+                                score-= 5;
+                                $('#incorrect-update').addClass('show-animation');
+                            }
+                        } else if(inputVal.length==1) {
+                            const str= String(minimaxValues[i-1]);
+                            const substr= str.substring(0, 1);
+                            if(substr !== inputVal){
+                                $(this).css('border', '2px solid red');
+                                score-= 5;
+                                $('#incorrect-update').addClass('show-animation');
+                            }
+                       }
+                    } else {
+                        if(inputVal == minimaxValues[i-1]) {
+                        $(this).prop('disabled', true);
+                        $(this).css('border', '2px solid grey');
+                        $('#correct-update').addClass('show-animation');
+                        $('#pointer-'+ i).css('visibility', 'hidden');
+                        $('#pointer-info-'+ i).css('visibility', 'hidden');
+                        createNewPointer(i+1)
+                        $('.minimax-value-' + (i+1)).prop('disabled', false);
+                        $('.minimax-value-' + (i+1)).focus();
+                        if(i == 7) showResults();
+                        } else {
+                          $(this).css('border', '2px solid red')
+                          score-= 5;
+                          $('#incorrect-update').addClass('show-animation');
+                        }
+                    }
+                    }
+              });
+          }
+
+        function createNewPointer(newIndex){
+            if(newIndex != 8){
+                if(newIndex==3 || newIndex==4){
+                    var newPointer = `<img id="pointer-${newIndex}" class="pointer" src="/static/images/pointer-left.png" alt="Navigation Pointer">`;
+                } else {
+                    var newPointer = `<img id="pointer-${newIndex}" class="pointer" src="/static/images/pointer-right.png" alt="Navigation Pointer">`;
+                }
+                var newPointerInfo= `<p id="pointer-info-${newIndex}" class="pointer-info">Next</p>`;
+                $('#pointer-container').append(newPointer);
+                $('#pointer-container').append(newPointerInfo);
+            }
+        }
+
+        function showResults(){
+            setTimeout(function() {
+            $('#basic-results').html('<h6>You Scored ' + score  + '%</h6>');
+            $('#next-container').css('display', 'block');
+            $('.basics-quiz-container').css('box-shadow', '0px 0px 10px #76c893');
+            $('#basic-results').css('color', '#76c893')
+            }, 2000);
+        }
+
+    //      $('#basic-results-btn').on('click', function() {
+    //            var minimaxValueAnswers = {
+    //            };
+    //            event.preventDefault();
+    //            let answersCorrect= 0;
+    //            for(let i=1; i<=minimaxValues.length; i++){
+    //                if($('.minimax-value-'+ i).val() != '') minimaxValueAnswers['question'+i]= $('.minimax-value-'+ i).val();
+    //                if($('.minimax-value-'+ i).val() == minimaxValues[i-1]) {
+    //                    answersCorrect +=1;
+    //                }
+    //            }
+    //            score= Math.floor((answersCorrect / minimaxValues.length) * 100);
+    //            $('#basic-results').html('<h6>You Scored ' + score  + '%</h6>');
+    //            if(score==100) {
+    //                $('#next-container').css('display', 'block');
+    //                $('.basics-quiz-container').css('box-shadow', '0px 0px 10px #76c893');
+    //                $('#basic-results').css('color', '#76c893')
+    //            } else {
+    //                $('#next-container').css('display', 'none');
+    //                $('.basics-quiz-container').css('box-shadow', '0px 0px 10px black');
+    //                $('#basic-results').css('color', 'black')
+    //            }
+    //
+    //            postData= {
+    //                answers: minimaxValueAnswers,
+    //                score: score
+    //            }
+    //
+    //            $.ajax({
+    //            url: '/save_answers_and_score',
+    //            type: 'POST',
+    //            contentType: 'application/json',
+    //            data: JSON.stringify(postData),
+    //            success: function(response) {
+    //                console.log('Answers and score saved successfully');
+    //            },
+    //            error: function(xhr, status, error) {
+    //                console.error('Error saving answers/score:', error);
+    //            }
+    //        });
+    //
+    //    })
+});
+
